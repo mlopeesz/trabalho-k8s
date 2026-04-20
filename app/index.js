5ae95e4bcd6e3b15f564c4592ac2f4b284fc8ab3
Mauricio Lopes <mlopeesz@gmail.com>
2026-04-20 20:51:14 -0300

    Add heavy processing endpoint to index.js

--- app/index.js
+++ app/index.js
@@ -12,3 +12,13 @@ app.get('/health', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor na porta 3000');
 });
+
+app.get('/heavy', (req, res) => {
+  let total = 0;
+
+  for (let i = 0; i < 1e8; i++) {
+    total += i;
+  }
+
+  res.send(`Processamento pesado: ${total}`);
+});
